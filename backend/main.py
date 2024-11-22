754a22a65bff1a7c4c8906b90bd1127bf7ea510c
fbb0 <bello2001felipe@gmail.com>
2024-11-22 12:32:06 +0100

    import uvicorn

--- backend/main.py
+++ backend/main.py
@@ -126,9 +126,12 @@ def draw_bounding_boxes(image, base_image_np, results):
     return image_with_boxes, detections
 
 
+
 if __name__ == "__main__":
+
     import os
-    port = int(os.environ.get("PORT", 8000))
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT isn't set
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 

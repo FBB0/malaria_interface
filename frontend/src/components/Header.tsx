75998cd3998285709a0a64d5d8603c80f4b8de8c
fbb0 <bello2001felipe@gmail.com>
2024-11-23 19:21:23 +0100

    update of the other visuals

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,3 @@
-const epochLogo = '/Epoch_Logo_Light.png'  // If using public folder
-
 export default function Header() {
   return (
     <header className="bg-[#333333] text-white p-8 lg:p-16">
@@ -7,12 +5,12 @@ export default function Header() {
         <div className="flex flex-col items-center gap-4 sm:gap-6">
           <div className="flex-shrink-0">
             <img 
-              src={epochLogo} 
+              src="/assets/Epoch_Logo_Light.png" 
               alt="Epoch" 
               className="h-6 lg:h-10"
               onError={(e) => {
                 console.error('Image failed to load:', e);
-                e.currentTarget.src = '/Epoch_Logo_Light.png';  // Fallback path
+                e.currentTarget.src = '/assets/Epoch_Logo_Light.png';
               }}
             />
           </div>
